5df9b53ec905ade283cd27d374ad3c07bc94feb4
Sujana Panta <31249618+suv9@users.noreply.github.com>
2018-09-17 17:52:06 -0500

    Update settings.py

--- mfscrm/settings.py
+++ mfscrm/settings.py
@@ -120,5 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-LOGIN_REDIRECT_URL = '/home'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+LOGIN_REDIRECT_URL = '/'
+
 
